4ce872cdf94ec6962e2c97ba11645782e39184ef
KugelbergDK <kugelberg.dk@gmail.com>
2018-10-25 13:41:56 +0200

    Added random starter

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Game {
     // Initialize modstandere
     Plant plante = new Plant();
@@ -5,7 +7,9 @@ public class Game {
 
 
     public void start(){
-        boolean zombieHit = true;
+        Random random = new Random();
+
+        boolean zombieHit = random.nextBoolean(); // Random boolean
 
         while(zombie.getHealth() != 0 || plante.getHealth() != 0){
             if (zombieHit){
